9b1a5c9430f11a3975d3d63540aaf448ca25c9e0
BoltSheep <thidampe@gmail.com>
2021-11-28 23:56:30 -0300

    Exercicio 3 finalizado

--- exercicio3.js
+++ exercicio3.js
@@ -1,7 +1,9 @@
 function verificaP(event) {
     if(event.parentNode.classList.contains('expandido')) {
         event.parentNode.classList.remove('expandido');
+        event.innerHTML = '+';
     } else {
         event.parentNode.classList.add('expandido');
+        event.innerHTML = '-';
     }
 }
